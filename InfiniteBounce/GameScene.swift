626aba286774a417370c638e5bcfc524e91cc555
rodriguesra <rodriguesra@hotmail.com>
2022-09-27 21:08:00 -0300

    Final commit

--- InfiniteBounce/GameScene.swift
+++ InfiniteBounce/GameScene.swift
@@ -17,7 +17,24 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     
     let sounds = (1...22).map { SKAction.playSoundFileNamed("\($0).wav", waitForCompletion: false) }
     var scoreFromCurrentBall = 0
-    var score = 0
+    
+    var score = 0 {
+        didSet {
+            scoreLabel.text = "Score: \(score)"
+        }
+    }
+    
+    let extraBallPoints = Set([10, 50, 100, 150, 250, 350, 500, 750, 1000, 1250, 1500, 2000, 2500, 3500, 5000])
+    
+    var numberOfBalls = 1 {
+        didSet {
+            numberOfBallsLabel.text = "Balls: \(numberOfBalls)"
+        }
+    }
+    
+    let topBar = SKSpriteNode(color: SKColor(white: 0.8, alpha: 1), size: CGSize(width: 500, height: 100))
+    let numberOfBallsLabel = SKLabelNode(fontNamed: "HelveticaNeue-Bold")
+    let scoreLabel = SKLabelNode(fontNamed: "HelveticaNeue-Bold")
     
     override func didMove(to view: SKView) {
         physicsBody = SKPhysicsBody(edgeLoopFrom: frame.insetBy(dx: 0, dy: -200))
@@ -27,6 +44,27 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         ballLauncher.yScale = 0.5
         ballLauncher.position = CGPoint(x: frame.midX, y: frame.maxY - 100)
         addChild(ballLauncher)
+        
+        topBar.position.y = frame.maxY - 50
+        addChild(topBar)
+        
+        scoreLabel.fontSize = 24
+        scoreLabel.fontColor = .black
+        scoreLabel.horizontalAlignmentMode = .right
+        scoreLabel.text = "Score: 0"
+        addChild(scoreLabel)
+        
+        scoreLabel.position = CGPoint(x: frame.maxX - 20, y: frame.maxY - 70)
+        scoreLabel.zPosition = 1
+        
+        numberOfBallsLabel.fontSize = 24
+        numberOfBallsLabel.fontColor = .black
+        numberOfBallsLabel.horizontalAlignmentMode = .left
+        numberOfBallsLabel.text = "Balls: 1"
+        addChild(numberOfBallsLabel)
+        
+        numberOfBallsLabel.position = CGPoint(x: frame.minX + 20, y: frame.maxY - 70)
+        numberOfBallsLabel.zPosition = 1
         
         advance()
     }
@@ -53,6 +91,23 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             launchBall(towards: location)
             ballLauncher.isHidden = true
             state = .bouncing
+            
+            var sequence = [SKAction]()
+            
+            for _ in 1..<numberOfBalls {
+                sequence.append(.wait(forDuration: 0.25))
+                sequence.append(SKAction.run {
+                    self.launchBall(towards: location)
+                })
+            }
+            
+            run(SKAction.sequence(sequence))
+            
+        case .gameOver:
+            if let scene = SKScene(fileNamed: "GameScene") {
+                scene.scaleMode = .resizeFill
+                view?.presentScene(scene, transition: .doorway(withDuration: 1))
+            }
             
         default:
             break
@@ -81,14 +136,52 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     }
     
     func createBouncer() {
-        let numberToCreate = 1
+        let positions = [-110, -55, 0.0, 55, 110]
+        let numberToCreate: Int
+        let positionsToCreate: [Double]
+        
+        if score < 5 {
+            numberToCreate = 1
+        } else if score < 200 {
+            numberToCreate = Int.random(in: 1...2)
+        } else {
+            numberToCreate = Int.random(in: 1...3)
+        }
+        
+        switch numberToCreate {
+        case 1:
+            positionsToCreate = [positions.randomElement() ?? 0]
+            
+        case 2:
+            let first = Int.random(in: 0..<positions.count)
+            var second = first + Int.random(in: 2...3)
+            if second >= positions.count { second -= positions.count}
+            positionsToCreate = [positions[first], positions[second]]
+            
+        default:
+            positionsToCreate = [positions[0], positions[2], positions[4]]
+            
+        }
         
         for i in 0..<numberToCreate {
             let bounceCount: Int
-            bounceCount = 1
+            
+            if score < 2 {
+                bounceCount = 1
+            } else if score == 3 {
+                bounceCount = 2
+            } else if score == 5 {
+                bounceCount = Int.random(in: 2...3)
+            } else if score < 10 {
+                bounceCount = Int.random(in: 2...4)
+            } else if score < 50 {
+                bounceCount = Int.random(in: 3...5)
+            } else {
+                bounceCount = Int.random(in: (score / 30)...(score / 25))
+            }
             
             let bouncer = BouncerNode(bounceCount: bounceCount)
-            bouncer.position = CGPoint(x: Double.random(in: -200...200), y: frame.minY - 50)
+            bouncer.position = CGPoint(x: positionsToCreate[i], y: frame.minY - 50)
             bouncer.physicsBody = SKPhysicsBody(circleOfRadius: 32)
             bouncer.physicsBody?.contactTestBitMask = 1
             bouncer.physicsBody?.restitution = 0.75
@@ -112,7 +205,13 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         }
         
         let checkForEnd = SKAction.run {
-            self.resetLauncher()
+            let hasOverlappingBouncers = self.children.contains { $0.name == "Bouncer" && $0.intersects(self.topBar) }
+            
+            if hasOverlappingBouncers {
+                self.gameOver()
+            } else {
+                self.resetLauncher()
+            }
         }
         
         run(SKAction.sequence([.wait(forDuration: 0.5), checkForEnd]))
@@ -135,10 +234,51 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         bouncer.hit()
         score += 1
         
+        if extraBallPoints.contains(score) {
+            bonusBall()
+        }
+        
         if scoreFromCurrentBall < 22 {
             scoreFromCurrentBall += 1
         }
         
         run(sounds[scoreFromCurrentBall - 1])
+    }
+    
+    func bonusBall() {
+        numberOfBalls += 1
+        
+        let node = SKSpriteNode(imageNamed: "extraBall")
+        node.zPosition = 10
+        node.xScale = 2
+        node.yScale = 2
+        node.alpha = 0
+        addChild(node)
+        
+        let appear = SKAction.group([SKAction.fadeIn(withDuration: 0.2), SKAction.scale(to: 1, duration: 0.2)])
+        let disappear = SKAction.group([SKAction.fadeOut(withDuration: 0.2), SKAction.scale(to: 0.5, duration: 0.2)])
+        node.run(SKAction.sequence([appear, .wait(forDuration: 0.5), disappear, .removeFromParent()
+                                   ]))
+    }
+    
+    func gameOver() {
+        guard state != .gameOver else { return }
+        state = .gameOver
+        
+        let bouncers = children.filter { $0.name == "Bouncer" }
+        
+        for bouncer in bouncers {
+            bouncer.run(SKAction.fadeOut(withDuration: 0.5))
+        }
+        
+        let node = SKSpriteNode(imageNamed: "gameOver")
+        node.zPosition = 100
+        node.xScale = 2
+        node.yScale = 2
+        node.alpha = 0
+        addChild(node)
+        
+        let appear = SKAction.group([SKAction.fadeIn(withDuration: 0.2), SKAction.scale(to: 1, duration: 0.2)])
+        node.run(appear)
     }
 }
